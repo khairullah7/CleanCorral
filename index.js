fc805f83fb4cb7490cd91af40f7c74b3c4eafdd5
Mo <129241013+khairullah7@users.noreply.github.com>
2024-04-11 04:36:02 +0000

    fix errors regarding form output

--- index.js
+++ index.js
@@ -8,7 +8,24 @@ const toggleDarkMode = () => {
   document.body.classList.toggle("dark-mode");
 }
 
-
 // TODO: Register a 'click' event listener for the theme button
 // Set toggleDarkMode as the callback function.
 themeButton.addEventListener("click", toggleDarkMode);
+
+// Add your query for the sign now button here
+let signNowButton = document.getElementById("sign-now-button");
+
+const addSignature = () => {
+  // Write your code to manipulate the DOM here
+  let nameA = document.getElementById("name").value;
+  let cityA = document.getElementById("city").value;
+
+  let signature = document.createElement("p");
+  signature.textContent = "🖊️ " + nameA + " from " + cityA + " supports this.";
+
+  let signatureSection = document.querySelector(".signatures");
+  signatureSection.appendChild(signature);
+}
+
+// Add a click event listener to the sign now button here
+signNowButton.addEventListener('click', addSignature);
