f904c1eb8038dd993add083781eeea7a23046906
Mo <129241013+khairullah7@users.noreply.github.com>
2024-04-12 22:26:55 +0000

    fix header and put background image in correct spot

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const toggleDarkMode = () => {
 
   // Write your code to manipulate the DOM here
   document.body.classList.toggle("dark-mode");
+
 }
 
 // TODO: Register a 'click' event listener for the theme button
