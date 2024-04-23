5e297e8f8d7b11b59bdd5a0b21ebd03c8059d4e5
Mo <129241013+khairullah7@users.noreply.github.com>
2024-04-23 04:00:50 +0000

    added animation for page in JS

--- index.js
+++ index.js
@@ -56,3 +56,33 @@ const validateForm = () => {
 }
 
 signNowButton.addEventListener('click', validateForm);
+
+let animations = {
+  revealDistance: 150,
+  initialOpacity: 0,
+  transitionDelay: 0,
+  transitionDuration: "2s",
+  transitionProperty: "all",
+  transitionTimingFunction: "ease"
+};
+
+let revealableContainers = document.querySelectorAll(".revealable");
+
+function reveal() {
+  let windowHeight = window.innerHeight;
+
+  for (let i = 0; i < revealableContainers.length; i++) {
+    let topOfRevealableContainer = revealableContainers[i].getBoundingClientRect().top;
+
+    if (topOfRevealableContainer < windowHeight - animations.revealDistance) {
+      revealableContainers[i].classList.add("active");
+    } else {
+      revealableContainers[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+
